5f345d65dc33f9cca25be31d011e6de6316417e6
Kesselyn <kesselynfortunato@hotmail.com>
2020-07-02 21:44:42 -0300

    Fix mensagem boas vindas

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -2,6 +2,6 @@
 module.exports = client => {
     client.on('guildMemberAdd', newMember => {
         const channel = newMember.guild.channels.cache.find(ch => ch.name === 'inicio');
-        channel.send(`Bem vindx ao servidor ${newMember}!\nMe chamo Cody e estarei ajudando aqui no Codando!\nMande a palavra **autenticar** no privado para que eu possa liberar o seu acesso aos outros canais do Codando 🙂\nCaso tenha problemas, mande mensagem no canal de #ajuda`);
+        channel.send(`Bem vindx ao servidor ${newMember}!\nMe chamo Cody e estarei ajudando aqui no Codando!\nMande mensagem para alguém da diretoria ou algum organizador pedindo para te autenticarem. Dessa forma você poderá ter acesso aos outros canais e conteúdos :)`);
     });
 }
