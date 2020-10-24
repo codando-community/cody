61c2d55d9bc156358829e81279cd79faa254d0b2
Maicon Souza <maiconsouzapa@gmail.com>
2020-10-24 14:57:13 -0300

    clean python code

--- README.md
+++
@@ -1,2 +0,0 @@
-# cody
-## só q em python
--- modules/ping.py
+++
@@ -1,6 +0,0 @@
-class Ping:
-    def __init__(self, cody):
-        self.cody = cody
-
-    async def on_message(message):
-        print('Message from {0.author}: {0.content}'.format(message))
