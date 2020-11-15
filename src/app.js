0466373cb39dd0183c2ef35c33c9d12fbe9e85a2
Maicon Souza <maiconsouzapa@gmail.com>
2020-11-15 16:29:41 -0300

    music player with bug: connect in the voice channel, sinalize audio emit, but no audio is emitted

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const Discord = require('discord.js')
+const { Player } = require("discord-player")
+require('dotenv').config()
+const app = express()
+
 const Welcome = require('./components/welcome')
 const Reactions = require('./components/reactions')
 const SendMessage = require('./components/sendMessage')
 const ServerLog = require('./components/serverLog')
 const Role = require('./components/role')
 const Forward = require('./components/sendMessage/forward')
-require('dotenv').config()
-const app = express()
+const MusicPlayer = require('./components/musicPlayer')
 
 const client = new Discord.Client()
 
 client.login(process.env.DS_TOKEN)
+
+client.player = new Player(client)
 
 client.on('ready', () =>
   console.log(process.env.SERVER_ID === '720760691677462588' ? 'Em ambiente de produção' : 'Em ambiente de desenvolvimento')
@@ -22,6 +27,7 @@ Reactions(client)
 SendMessage(client)
 ServerLog(client)
 Role(client)
+MusicPlayer(client)
 
 app.listen(8080, () => { })
 
