1ae3539badf3b449ecee04eef25bccd7822fc40d
Maicon Souza <maiconsouzapa@gmail.com>
2020-11-17 18:06:28 -0300

    bug de notificação de boas vindas corrigido

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -4,7 +4,7 @@ module.exports = client => client.on('guildMemberAdd', member => {
     .filter(server => server.id === process.env.SERVER_ID)
     .map(server =>
       server.channels.cache
-      .filter(channel => channel.id === process.env.INICIO_ID)
+      .filter(channel => channel.id === process.env.CHANNEL_INICIO_ID)
       .map(channel =>
         channel.send(`Bem vindx ao servidor ${member}!\nMe chamo Cody e estarei ajudando aqui no Codando!\nMande mensagem para alguém da diretoria ou algum organizador pedindo para te autenticarem. Dessa forma você poderá ter acesso aos outros canais e conteúdos :)`)
       )
