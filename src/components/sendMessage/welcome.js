2fb1e2081064b8e7217815798e8b0b327f27a354
Maicon Souza <maiconsouzapa@gmail.com>
2020-12-02 14:10:34 -0300

    welcome bug fix

--- src/components/sendMessage/welcome.js
+++ src/components/sendMessage/welcome.js
@@ -4,12 +4,14 @@ module.exports = (client, activeServer) => client.on('guildMemberAdd', member =>
     .filter(server => server.id === activeServer.server_id)
     .map(server =>
       server.channels.cache
-      .filter(channel => channel.id === process.env.CHANNEL_INICIO_ID)
+      .filter(channel => channel.id === activeServer.text_channel.bem_vindo)
       .map(channel =>
-        channel.send(`
-        Bem vindx ao servidor ${member}, aqui é o Cody. Fico muito feliz que você tenha aceitado fazer parte da nossa comunidade!\n
-        Para que você tenha acesso aos demais conteúdos do server, preciso que chame alguém da diretoria para te autenticar. Dessa forma confirmaremos que você é um aluno e te daremos acesso aos outros canais e conteúdos.\n
-        **Que a força da comunidade esteja com você!**🙂`)
+        channel.send(
+        `Bem vindx ao servidor ${member}, aqui é o Cody.\n`
+        + 'Fico muito feliz que você tenha aceitado fazer parte da nossa comunidade!\n'
+        + 'Para que você tenha acesso aos demais conteúdos do server, preciso que chame alguém da diretoria para te autenticar. Dessa forma confirmaremos que você é um aluno e te daremos acesso aos outros canais e conteúdos.\n'
+        + '**Que a força da comunidade esteja com você!** 🙂'
+        )
       )
     )
 
