78fbc134bbe389418530b9aacd2417dd2235caeb
Maicon Souza <maiconsouzapa@gmail.com>
2021-01-12 20:01:08 -0300

    here we go again

--- src/events/guildMemberRemove/goodbye.js
+++ src/events/guildMemberRemove/goodbye.js
@@ -2,9 +2,9 @@ module.exports = (client, activeServer, member) => {
 
   client.guilds.cache
     .find(server => server.id === member.guild.id)
-      ?.channels.cache
-        ?.find(channel => channel.id === activeServer.text_channel.mensagens_cody)
-          ?.send(
+      .channels.cache
+        .find(channel => channel.id === activeServer.text_channel.mensagens_cody)
+          .send(
             `O aluno *${member.user.username}#${member.user.discriminator}* saiu do server!`
           )
 
