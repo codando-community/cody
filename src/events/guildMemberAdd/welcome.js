1c54325cad03ab8d59ac02fc46f41dcf6d780270
Maicon Souza <maiconsouzapa@gmail.com>
2021-01-12 20:07:33 -0300

    here we go again

--- src/events/guildMemberAdd/welcome.js
+++ src/events/guildMemberAdd/welcome.js
@@ -4,9 +4,9 @@ module.exports = (client, activeServer, member) => {
 
   client.guilds.cache
     .find(server => server.id === member.guild.id)
-      ?.channels.cache
-        ?.find(channel => channel.id === activeServer.text_channel.bem_vindo)
-          ?.send(`${member} entrou`)
+      .channels.cache
+        .find(channel => channel.id === activeServer.text_channel.bem_vindo)
+          .send(`${member} entrou`)
 
   member.send('Olá, sou o Cody e tals, Você prefere ser autenticado por:\n 1 - mim\n 2 - um humano')
 
