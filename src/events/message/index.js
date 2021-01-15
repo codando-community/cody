3ded3fa77f83f5dfcd08f404ef51ea4ab7aae8eb
Davi Fonseca <davi.brandao2013@gmail.com>
2021-01-15 00:22:40 -0300

    Auth funcionando, mas requer alguns refinamentos.

--- src/events/message/index.js
+++ src/events/message/index.js
@@ -1,23 +1,26 @@
 module.exports = (client, activeServer) => client.on('message', msg => {
   if (msg.author !== client.user) {
     if (msg.channel.type === 'dm') {
-      console.log('msg.content: ', msg.content.toLowerCase())
+      //console.log('msg.content: ', msg.content.toLowerCase())
       if (msg.content.toLowerCase().indexOf('autenticar ') !== -1) {
         const Action = require('./sendMessage/auth')
         Action(client, activeServer, msg)
 
       } else if (msg.content.toLowerCase().indexOf('conversar com um organizador') !== -1) {
         const Action = require('./sendMessage/forward')
-        console.log('activeServer.text_channel.auth: ', activeServer.text_channel.auth)
+        //console.log('activeServer.text_channel.auth: ', activeServer.text_channel.auth)
         Action(client, activeServer, msg, activeServer.text_channel.auth)
 
-      } else if (msg.content.toLowerCase().indexOf('sim, está correto') !== -1) {
+      } else if (msg.content.toLowerCase().indexOf('sim, está correto ') !== -1) {
         const Action = require('./sendMessage/auth')
         Action(client, activeServer, msg)
+        //Executar apenas depois da anterior o envio de uma mensagem no canal 'caixa-de-entrada' contendo o seguinte texto 'Aluno davibrandao18#9821 autenticado com sucesso!'
+        //Action = require('./sendMessage/forward')
+        //Action(client, activeServer, msg, activeServer.text_channel.mensagens_cody)
 
       } else if (msg.content.toLowerCase().indexOf('não, há divergências') !== -1) {
         const Action = require('./sendMessage/forward')
-        console.log('activeServer.text_channel.auth: ', activeServer.text_channel.auth)
+        //console.log('activeServer.text_channel.auth: ', activeServer.text_channel.auth)
         Action(client, activeServer, msg, activeServer.text_channel.auth)
 
       } else {
--- src/events/message/sendMessage/auth.js
+++ src/events/message/sendMessage/auth.js
@@ -3,7 +3,42 @@ const instance = require('../../../database');
 //TODO adicionar link com pagina publia do notion ajudando no processo de autenticação
 module.exports = (client, activeServer, msg) => {
   const RA = msg.content.toLowerCase().split('autenticar ')[1]
+  //-----------------------[START] - CODIGO DAVI-------------------------//
+  if (msg.content.toLowerCase().indexOf('sim, está correto ') !== -1) {
+    const ra = msg.content.toLowerCase().split('sim, está correto ')[1]
+    instance
+      .select({ registration: Number(ra) })
+      .then(result => {
+        let { type } = result[0]
 
+        switch (type) {
+          case 'organizador':
+              type = 'Veterano'
+            break;
+          case 'pre_calouro':
+              type = 'Pre-calouro'
+            break;
+
+          case 'calouro':
+              type = 'Calouro'
+            break;
+        }
+      //-------------------------------------------------//
+      let role = client.guilds.cache.find(g => g.id === activeServer.server_id).roles.cache.find(role => role.name === type)
+      let member = client.guilds.cache.find(g => g.id === activeServer.server_id).members.cache.find(m => m.id === msg.channel.recipient.id)
+
+      member.roles.add(role)
+        .then(
+          msg.reply(`Seja bem vindo a nossa comunidade ! O seu acesso aos canais já foi liberado.`
+          +`\nQue a força da comunidade esteja com você :vulcan:`)
+        )
+        .catch(console.error)
+      })
+      .catch((err) => {
+        console.trace(err)
+      });
+    }
+  //-----------------------[END] - CODIGO DAVI-------------------------//
   instance
     .select({ registration: Number(RA) })
     .then(result => {
@@ -11,7 +46,7 @@ module.exports = (client, activeServer, msg) => {
       msg.reply(
         `Você está cursando a faculdade na **${university}**, na unidade **${campus}**?` +
         '\n**Responda copiando** exatamente uma das frases a seguir e me enviando' +
-        '\nSim, está correto' +
+        '\nSim, está correto SEU_RA' +
         '\nNão, há divergências'
       )
     })
@@ -23,5 +58,4 @@ module.exports = (client, activeServer, msg) => {
         '\n\nDúvidas sobre autenticação? clique no link: [link do notion]'
       );
     });
-
 }
