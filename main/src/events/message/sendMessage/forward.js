de4bfabf84642b7651f1853f8b72a57d4fa799ce
Maicon Souza <maiconsouzapa@gmail.com>
2021-05-21 20:14:44 -0300

    Melhoria da integração com Watson, novas variaveis de ambiente e clean code

--- main/src/events/message/sendMessage/forward.js
+++ main/src/events/message/sendMessage/forward.js
@@ -3,9 +3,11 @@ const AssistantV2 = require('ibm-watson/assistant/v2');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 const addRole = (client, msg, idMember, roleName, activeServer) => {
-  let cargoInstituicao = client.guilds.cache.find(g => g.id === activeServer.server_id).roles.cache.find(role => role.name.indexOf(roleName) !== -1)
-  let cargoMembro = client.guilds.cache.find(g => g.id === activeServer.server_id).roles.cache.find(role => role.name.indexOf('membro') !== -1)
-  let member = client.guilds.cache.find(g => g.id === activeServer.server_id).members.cache.find(m => m.id === idMember)
+  const guildsCache = client.guilds.cache.find(g => g.id === activeServer.server_id)
+
+  let cargoInstituicao = guildsCache.roles.cache.find(role => role.name.indexOf(roleName) !== -1)
+  let cargoMembro = guildsCache.roles.cache.find(role => role.name.indexOf('membro') !== -1)
+  let member = guildsCache.members.cache.find(m => m.id === idMember)
 
   cargoMembro &&
     member.roles.add(cargoMembro)
@@ -25,15 +27,15 @@ const addRole = (client, msg, idMember, roleName, activeServer) => {
 const assistant = new AssistantV2({
   version: '2020-04-01',
   authenticator: new IamAuthenticator({
-    apikey: 'VYVVtGoRGynFLa-L0qicba9fKoDSOEKJrz6ms_dhmjRu',
+    apikey: process.env.ASSISTANT_APIKEY,
   }),
   disableSslVerification: true,
-  url: 'https://gateway.watsonplatform.net/assistant/api',
+  url: process.env.ASSISTANT_URL,
 });
 
 function messageFlow(msg, client, activeServer) {
   assistant.message({
-    assistantId: 'd0f3d408-0f3c-4621-9f7f-b4f536096326',
+    assistantId: process.env.ASSISTANT_ID_DM,
     sessionId: store.get(msg.author.id).session_id,
     input: {
       message_type: 'text',
@@ -43,7 +45,7 @@ function messageFlow(msg, client, activeServer) {
     .then(res => {
       if (res.result.output.generic[0] && res.result.output.generic[0].text.indexOf('discord_id') !== -1) {
         assistant.message({
-          assistantId: 'd0f3d408-0f3c-4621-9f7f-b4f536096326',
+          assistantId: process.env.ASSISTANT_ID_DM,
           sessionId: store.get(msg.author.id).session_id,
           input: {
             message_type: 'text',
@@ -76,7 +78,7 @@ function messageFlow(msg, client, activeServer) {
 
 function createSession(msg) {
   assistant.createSession({
-    assistantId: 'd0f3d408-0f3c-4621-9f7f-b4f536096326',
+    assistantId: process.env.ASSISTANT_ID_DM,
   })
     .then(res => {
       store.set(msg.author.id, {
