a13980ab6ebe0a4832f9dd7df604b3ae54699f0c
Maicon Souza <maiconsouzapa@gmail.com>
2021-05-22 02:14:07 -0300

    refatoração do forward -> dm, clean code, documentação e mapeamento de TODOs e itens não utilizáveis

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
-const ENV_MODE = 'prod'
+/**
+ * Environment mode
+ */
+const ENV_MODE = 'dev'
 module.exports = ENV_MODE
 
 require('dotenv').config();
-
 const Discord = require('discord.js');
 const express = require('express');
 const Express = express();
 
+// Import events
 const GuildMemberAdd = require('./events/guildMemberAdd')
 const GuildMemberRemove = require('./events/guildMemberRemove')
 const Message = require('./events/message')
 const Ready = require('./events/ready')
 
+// Client config
 const client = new Discord.Client();
 const config = require('./config.json');
-const activeServer = config[ENV_MODE === 'dev' ? 'dev' : 'prod'];
+const activeServer = config[ENV_MODE];
 client.login(process.env.DS_TOKEN);
 
+// Event call
 Ready(client, activeServer, config)
 Message(client, activeServer)
 GuildMemberAdd(client, activeServer)
--- src/events/message/index.js
+++ src/events/message/index.js
@@ -1,49 +1,55 @@
 module.exports = (client, activeServer) => client.on('message', msg => {
-
   if (msg.author !== client.user) {
+
     if (msg.channel.type === 'dm') {
-        const Action = require('./sendMessage/forward')
-        Action(client, activeServer, msg, activeServer.text_channel.mensagens_cody)
+      const DM = require('./sendMessage/dm')
+      DM(client, activeServer, msg)
 
-    } else if (msg.channel.id === activeServer.text_channel.avisos || msg.channel.id === activeServer.text_channel.memes) {
+    } else if (msg.channel.id === activeServer.text_channel.avisos) {
+      const Avisos = require('./reactions/avisos')
+      Avisos(client, activeServer, msg)
 
-      const Reaction = msg.channel.id === activeServer.text_channel.avisos
-        ? require('./reactions/avisos')
-        : require('./reactions/meme')
-      Reaction(client, activeServer, msg)
+    } else if (msg.channel.id === activeServer.text_channel.memes) {
+      const Memes = require('./reactions/meme')
+      Memes(client, activeServer, msg)
 
-    } else if (msg.channel.id === activeServer.text_channel.cody_bash && msg.content.indexOf('--')) {
-      let Path
+    } else if (msg.channel.id === activeServer.text_channel.cody_bash &&
+                      msg.content.includes('--')) {
+
       switch (msg.content.toLowerCase().split(' ')[0]) {
         case 'role':
-          Path = require('./role/RoleManager')
-          Path(client, activeServer, msg)
+          let RoleManager = require('./role/RoleManager')
+          RoleManager(client, activeServer, msg)
           break;
 
         case 'read':
-          Path = require('./sendMessage/read')
-          Path(msg)
+          let Read = require('./sendMessage/read')
+          Read(msg)
           break;
 
         case 'send':
-          Path = require('./sendMessage/userAndRoles')
-          Path(client, msg)
+          let UserAndRoles = require('./sendMessage/userAndRoles')
+          UserAndRoles(client, msg)
           break;
 
         case 'voice':
-          Path = require('./audio')
-          Path(client, activeServer, msg)
+          let Voice = require('./audio')
+          Voice(client, activeServer, msg)
           break;
 
         case 'server':
-          Path = require('./serverStatus/roleStatus')
-          Path(client, activeServer, msg)
+          let Server = require('./serverStatus/roleStatus')
+          Server(client, activeServer, msg)
           break;
 
-        case 'auth':
-          Path = require('./auth')
-          Path(client, activeServer, msg)
-          break;
+        // desabilitado devido a não ter necessidade de autenticar manualmente e se for o caso, ter que refatorar o código devido a alterações de BD
+        // case 'auth':
+        //   ManualAuth = require('./manualAuth')
+        //   ManualAuth(client, activeServer, msg)
+        //   break;
+
+        default:
+          msg.reply("opção inválida")
       }
 
     }
