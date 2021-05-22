b0d05d72271338fe448f19f9c0eb49c9580de378
Maicon Souza <maiconsouzapa@gmail.com>
2021-05-22 11:18:09 -0300

    base de script ibm function

---
+++ functions/add-role/webpack.common.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
