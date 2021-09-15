4484572be6d3306ceb0b466ebd63285fff56898b
Luana Otsuka <otsuka.luana@gmail.com>
2021-09-15 20:00:37 -0300

    Criacão de funcao para listar emojis e adicao de emojis faltantes no canal de avisos

--- src/events/message/reactions/avisos.js
+++ src/events/message/reactions/avisos.js
@@ -1,9 +1,9 @@
 module.exports = (client, activeServer, msg) => {
   let emojiIDs = [
-    '768621446392971277', //LogoCodando
     '768621842688638986', //MayTheCommunity
-    '768623311323529216', //VivaUmBugDeCadaVez
     '768622316648136714', //CodandoCommunity
+    '823986197960851497', //may_the_community
+    '823719899486617680' //codando
   ]
 
   msg.channel.id === activeServer.text_channel.avisos
@@ -11,5 +11,4 @@ module.exports = (client, activeServer, msg) => {
     && client.emojis.cache
       .filter(emoji => emojiIDs.indexOf(emoji.id) > -1)
       .map(emoji => msg.react(emoji))
-
 }
